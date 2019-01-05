feb7adf9e3448701d8b2957ef8d6e1f34a59bcf7
mindtree476 <mindtree476@Selimind73.iiht.tech>
2019-01-05 19:06:02 +0530

    RunnerAsTestNG

--- CucumberFramework/src/test/java/runner/MainRunner.java
+++ CucumberFramework/src/test/java/runner/MainRunner.java
@@ -1,11 +1,11 @@
 package runner;
 
 import org.junit.runner.RunWith;
+import cucumber.api.testng.*;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/java/resources/features",
 		glue = "stepDefinition",
@@ -13,6 +13,6 @@ import cucumber.api.junit.Cucumber;
 		dryRun = false,
 		plugin = {"pretty","html:target/cucumber","json:target/cucumber.json","com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}
 		)
-public class MainRunner {
+public class MainRunner extends AbstractTestNGCucumberTests{
 
 }
