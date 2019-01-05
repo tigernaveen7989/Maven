7c4304efd283ec003210984f04adb5ef38cfb193
mindtree476 <mindtree476@Selimind73.iiht.tech>
2019-01-05 18:51:05 +0530

    testng.xml

--- CucumberFramework/src/main/java/stepDefinition/MasterHooks.java
+++ CucumberFramework/src/main/java/stepDefinition/MasterHooks.java
@@ -1,5 +1,9 @@
 package stepDefinition;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import utils.DriverFactory;
@@ -12,10 +16,21 @@ public class MasterHooks extends DriverFactory{
 	}
 	
 	@After()
-	public void tearDown() throws Exception{
-		if(driver != null) {
-			driver.manage().deleteAllCookies();
-			driver.quit();
+	public void tearDown(Scenario scenario) throws Exception{
+		try {
+			if(driver != null && scenario.isFailed()) {
+				scenario.embed(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES), "image/png");
+				driver.manage().deleteAllCookies();
+				driver.quit();
+			}
+			
+			if(driver != null) {
+				driver.manage().deleteAllCookies();
+				driver.quit();
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
