ebb5ba3b48c9ab2c45b4a9ebac534fc2449abcca
Naveen Kumar <naveen85918@gmail.com>
2019-09-13 16:01:49 +0530

    test-suite

--- test-suite/ecommerce/src/main/java/com/shell/markethub/ecommerce/pageobjects/AddToYourOrderPage.java
+++ test-suite/ecommerce/src/main/java/com/shell/markethub/ecommerce/pageobjects/AddToYourOrderPage.java
@@ -5,6 +5,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+/**
+ * 
+ * @author N.Kumar8
+ *
+ */
 public class AddToYourOrderPage extends eCommerceBasePageObject{
 
 	public AddToYourOrderPage() {
